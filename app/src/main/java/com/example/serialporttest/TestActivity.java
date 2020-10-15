e51cba42034bbd75800a0b5b400300cbf6e7e989
mingchong <mingchong2005@sina.com>
2020-10-15 09:01:30 +0800

    F39 LORA

--- app/src/main/java/com/example/serialporttest/TestActivity.java
+++ app/src/main/java/com/example/serialporttest/TestActivity.java
@@ -71,13 +71,20 @@ public class TestActivity extends SerialPortActivity {
 		public void onClick(View v) {
 			int i;	
 			String test = "sip get_ver";
-			
+			//String test = {68, 25, 01, 01, 95, C8, 00, 01, 01, 10 }; //"sip get_ver";
+			//int[] buffer = {0x68, 0x25, 0x01, 0x01, 0x95, 0xC8, 0x00, 0x01, 0x01, 0x10};
+			//int[] buffer = {0x68, 0x24, 0x01, 0x01, 0x95, 0xC9, 0x00, 0x01, 0x01, 0x10};
+			try {
+				mOutputStream.write(new String(test).getBytes());
 				try {
-					mOutputStream.write(new String(test).getBytes());
-					//mOutputStream.write('0x0d');
-				} catch (IOException e) {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+				//mOutputStream.write('0x0d');
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			  }
 	};
 
@@ -86,23 +93,20 @@ public class TestActivity extends SerialPortActivity {
 		public void onClick(View v) {
 			int i;	
 			String test = "sip get_hw_model";
-			String ConvertString = null;
-			CharSequence editText = mInputText.getText();
+			//String ConvertString = null;
+			//CharSequence editText = mInputText.getText();
 
-			if(editText == null || editText.length() == 0){
-				mReception.setText("数据为空，写入失败");
-			}
-			else{
-				ConvertString = editText.toString();
-				Log.e(TAG, "ConvertString = " + ConvertString);
-
+			try {
+				mOutputStream.write(new String(test).getBytes());
 				try {
-					mOutputStream.write(new String(ConvertString).getBytes());
-					//mOutputStream.write('0x0d');
-				} catch (IOException e) {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			  }
+				//mOutputStream.write('0x0d');
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	};
 
@@ -224,6 +228,34 @@ public class TestActivity extends SerialPortActivity {
 		}
 	};
 
+	public static String hexStringToString(String hexStr) {
+		String str = "0123456789ABCDEF";
+		char[] hexs = hexStr.toCharArray();
+		byte[] bytes = new byte[hexStr.length() / 2];
+		int n;
+		for (int i = 0; i < bytes.length; i++) {
+			n = str.indexOf(hexs[2 * i]) * 16;
+			n += str.indexOf(hexs[2 * i + 1]);
+			bytes[i] = (byte) (n & 0xff);
+		}
+		return new String(bytes);
+	}
+
+	private static String bytesToHexString(byte[] src) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (src == null || src.length <= 0) {
+			return null;
+		}
+		char[] buffer = new char[2];
+		for (int i = 0; i < src.length; i++) {
+			buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+			buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+			System.out.println(buffer);
+			stringBuilder.append(buffer);
+		}
+		return stringBuilder.toString();
+	}
+
 
 	@Override
 	protected void onDataReceived(final byte[] buffer, final int size) {
@@ -233,11 +265,20 @@ public class TestActivity extends SerialPortActivity {
 					//String strReceive = convertHexToString(byteToHexString(buffer,size));
 					//Log.i(TAG, "TestActivity ---> onDataReceived" + strReceive);
 					//JudgeResult(strReceive);
+				Log.i(TAG, "TestActivity ---> onDataReceived" + buffer);
+				String HexString = bytesToHexString(buffer);
+				Log.e(TAG, "HexString.getBytes() = " + HexString);
+				Log.e(TAG, "HexString length =" + HexString.length());
 
-					if (mReception != null) {  
-                        mReception.append(new String(buffer, 0, size));  
-                        }  
+				if (mReception != null) {
+					mReception.append(new String(buffer, 0, size));
+				}
+
+				//if (mReception != null) {
+                //        mReception.append(HexString);
+                //        }
 			}
-		});
+		}
+		);
 	}
 }
